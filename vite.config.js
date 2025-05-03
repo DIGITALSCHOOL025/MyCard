bf852c8a0d66ce64b048ee7d170f5f336cb8cfb0
Laurent Mamba <mambalaurent009@gmail.com>
2025-05-03 19:53:19 +0200

    Correction du fichier vite.config.js pour Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ou '/' selon le contexte, voir ci-dessous
 })
+
